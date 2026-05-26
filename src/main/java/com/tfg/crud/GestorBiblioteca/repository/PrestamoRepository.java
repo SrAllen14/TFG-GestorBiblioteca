55dd5511dbde49de121942436b3d44ee21147646
Alvaro <alvaro.allper.1@educa.jcyl.es>
2026-05-26 20:33:53 +0200

    Correciones minimas

--- src/main/java/com/tfg/crud/GestorBiblioteca/repository/PrestamoRepository.java
+++ src/main/java/com/tfg/crud/GestorBiblioteca/repository/PrestamoRepository.java
@@ -5,7 +5,6 @@
 package com.tfg.crud.GestorBiblioteca.repository;
 
 import com.tfg.crud.GestorBiblioteca.entity.Prestamo;
-import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
