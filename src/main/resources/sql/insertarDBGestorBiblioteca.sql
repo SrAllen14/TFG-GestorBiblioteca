d7c53ceac1c9c02e5af839857b32e2ae11131429
Alvaro <alvaro.allper.1@educa.jcyl.es>
2026-06-10 18:10:33 +0200

    correcciones varias

---
+++ src/main/resources/sql/insertarDBGestorBiblioteca.sql
@@ -0,0 +1,5 @@
+USE DBGestorBiblioteca;
+
+INSERT INTO usuario (apellido1,apellido2,nombre,password,username,rol,dni, estado_usuario)
+VALUES ('Admin','Test','Usuario',
+    '$2a$10$dsfJPvp2giwB7tTXF5zUU.Cqofj6DP1Usn.IAIDXDw1BAvGjQMGUe','admin','ROLE_ADMINISTRADOR','99999999R','ACTIVO');
